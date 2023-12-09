5aa0ae0ce32a8343f28d7a84502365ddbb18eb2e
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-09 23:35:55 +0100

    pdf plotting in `utils.sh`; build command

--- utils.sh
+++ utils.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-initialMetArray=(0.1 0.2)
-initialAdoMetArray=(0.3 0.4)
-initialAdoHcyArray=(0.5 0.6)
-initialHcyArray=(0.7 0.8)
-metinArray=(0.9 1.0)
+graphExt=pdf
 
 buildDir="$(pwd)/build"
 datDir="$(pwd)/graphs/dat"
-pngDir="$(pwd)/graphs/png"
+pngDir="$(pwd)/graphs/${graphExt}"
 
 
 buildSimlib() {
@@ -61,14 +57,41 @@ createDataset() {
     mkdir -p "$pngDir"
   fi
 
-  runImsWithParams "50" "10" "5" "2" "50"
+  #                MET AdoMet AdoHcy Hcy MetinRate
+  runImsWithParams "50" "10"   "5"   "2"   "50"
+#  runImsWithParams "50" "10" "5" "2" "50"
 
-  echo "CREATING PNG FILES..."
-  for datFile in "$datDir"/*.dat; do
-    local pngFile="${datFile%.dat}.png"
-    gnuplot -e "set terminal png; set output '$pngFile'; set title 'Lorenz Equation Output'; set xlabel 'Time'; set ylabel 'Values'; set datafile separator whitespace; plot '$datFile' using 1:2 with lines title 'Met', '$datFile' using 1:3 with lines title 'AdoMet', '$datFile' using 1:4 with lines title 'AdoHcy', '$datFile' using 1:5 with lines title 'Hcy'"
+  echo "CREATING PNG FILES from" "$datDir"/*.dat
+  for datFile in "$datDir"/*.dat ; do
+    local graphFile="${datFile%.dat}.${graphExt}"
+    echo "Creating $graphFile..."
+gnuplot -e "set terminal pdf size 29.7cm, 84.1cm; \
+            set output '${graphFile}'; \
+            set multiplot layout 5,1 title 'Biology Data Overview'; \
+            set datafile separator whitespace; \
+            set lmargin 10; \
+            set rmargin 10; \
+            set tmargin 2; \
+            set bmargin 2; \
+            set title 'Met'; \
+            set ylabel 'µM'; \
+            plot '$datFile' using 1:2 with lines title 'Met'; \
+            set title 'AdoMet'; \
+            set ylabel 'µM'; \
+            plot '$datFile' using 1:3 with lines title 'AdoMet'; \
+            set title 'AdoHcy'; \
+            set ylabel 'µM'; \
+            plot '$datFile' using 1:4 with lines title 'AdoHcy'; \
+            set title 'Hcy'; \
+            set ylabel 'µM'; \
+            plot '$datFile' using 1:5 with lines title 'Hcy'; \
+            set title 'Metin'; \
+            set ylabel 'µM'; \
+            plot '$datFile' using 1:6 with lines title 'Metin'; \
+            unset multiplot; \
+            set output;"
   done
-  mv "$datDir"/*.png "$pngDir"
+  mv "$datDir"/*.$graphExt "$pngDir"
 }
 
 createArchive() {
@@ -86,6 +109,9 @@ createArchive() {
 main() {
   case $1 in
     build)
+      buildProject
+      ;;
+    build_all)
       buildSimlib
       buildProject
       ;;
@@ -97,7 +123,7 @@ main() {
       createDataset
       ;;
     *)
-      echo "Usage: $0 [build|archive <archiveName>|create_dataset]"
+      echo "Usage: $0 [build_all|build|archive <archiveName>|create_dataset]"
       ;;
   esac
 }
