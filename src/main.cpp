36fcdd444fd78d6aa1fe33c01bcc85dfbf2b9f29
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-10 12:49:01 +0100

    it works, but better

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,33 @@
 constexpr double MU = 1e-6;
 double _5mthf = 5.2 * MU;
 
-constexpr double V_mat1_max = 561 * MU;
+constexpr double HOUR = 3600;
+constexpr double V_mat1_max = (561 * MU) / HOUR;
 constexpr double K_mat1_m = 41 * MU;
 constexpr double K_mat1_i = 50 * MU;
 
-constexpr double V_mat3_max = 22870 * MU;
+constexpr double V_mat3_max = (22870 * MU) / HOUR;
 constexpr double K_mat3_m2 = 21.1 * MU;
 
-constexpr double V_gnmt_max = 10600 * MU;
+constexpr double V_gnmt_max = (10600 * MU) / HOUR;
 constexpr double K_gnmt_m = 4500 * MU;
 constexpr double K_gnmt_i = 20 * MU;
 
-constexpr double V_meth_max = 4521 * MU;
+constexpr double V_meth_max = (4521 * MU) / HOUR;
 constexpr double K_meth_m2__A = 10 * MU;
 
-constexpr double HOUR = 3600;
 const double alpha_1 = 100 / HOUR;
 const double alpha_2 = 10 / HOUR;
 
 const double beta_1 = 1.7 / HOUR;
 const double beta_2 = 30 / HOUR;
 
-constexpr double V_ms_max = 500 * MU;
+constexpr double V_ms_max = (500 * MU) / HOUR;
 constexpr double K_ms_m_hcy = 0.1 * MU;
 constexpr double K_ms_m5mthf = 25 * MU;
 constexpr double K_ms_d = 1 * MU;
 
-constexpr double V_bhmt_max = 2500 * MU; // velocity. µ / h
+constexpr double V_bhmt_max = (2500 * MU) / HOUR; // velocity. µ / h
 constexpr double K_bhmt_m = 12 * MU;
 
 
@@ -61,7 +61,6 @@ double Function3::Value() {
   return ret;
 }
 
-uint a = 0;
 inline double V_ms(const double hcy) {
   // Michaelis-Menten equation implementation
   double numerator = V_ms_max * _5mthf * hcy;
@@ -69,8 +68,7 @@ inline double V_ms(const double hcy) {
   double denominator1 = K_ms_m_hcy * (K_ms_d + _5mthf);
   double denominator2 = hcy * (K_ms_m5mthf + _5mthf);
   double denominator = denominator1 + denominator2;
-  if ((++a % 100000) == 0)
-      std::cerr << "V MS " << numerator / denominator << std::endl;
+
   return numerator / denominator;
 }
 
@@ -89,14 +87,11 @@ inline double K_mat3_m1(const double adoMet) {
   return 20000 / (1 + 5.7 * (adoMet_exp * adoMet_exp));
 }
 
-uint b = 0;
 inline double V_mat1(const double met, const double adoMet) {
   const double denominator1 = (K_mat1_m / met) * (1 + adoMet / K_mat1_i);
   const double denominator = 1 + denominator1;
 
   assert(denominator != 0 && "Denominiator must not be zero");
-    if ((++b % 100000) == 0)
-        std::cerr << "V MAT1 " << V_mat1_max / denominator << std::endl;
 
   return V_mat1_max / denominator;
 }
@@ -105,8 +100,6 @@ uint c = 0;
 inline double V_mat3(const double met, const double adoMet) {
   const double denominator = 1 + (K_mat3_m1(adoMet) * K_mat3_m2) / (met * (met + K_mat3_m2));
 
-    if ((++c % 100000) == 0)
-        std::cerr << "V MAT3 " << V_mat3_max / denominator << std::endl;
   return V_mat3_max / denominator;
 }
 
@@ -189,14 +182,14 @@ struct MetabolicModel {
     fn_getTimeExp(Input(Hcy), getTimeExp),
 
 //    Metin(metinMax * MU * fn_getTimeLog),
-    Metin(metinMax * MU * fn_getTimeExp),
-//    Metin(metinMax * MU),
+//    Metin(metinMax * MU * fn_getTimeExp),
+    Metin(metinMax * MU * 3e-4),
 
       // TODO if `* MU`, then we got weird graphs. FUCK. Nevermind...
-    Met(fn_V_ms + fn_V_bhmt + Metin - fn_V_mat1 - fn_V_mat3, initialMet),
-    AdoMet(fn_V_mat1 + fn_V_mat3 - fn_V_meth - fn_V_gnmt, initialAdoMet),
-    AdoHcy(fn_V_meth + fn_V_gnmt - fn_V_ah, initialAdoHcy),
-    Hcy(fn_V_ah - fn_V_cbs - fn_V_ms - fn_V_bhmt, initialHcy) {}
+    Met(fn_V_ms + fn_V_bhmt + Metin - fn_V_mat1 - fn_V_mat3, initialMet * MU),
+    AdoMet(fn_V_mat1 + fn_V_mat3 - fn_V_meth - fn_V_gnmt, initialAdoMet * MU),
+    AdoHcy(fn_V_meth + fn_V_gnmt - fn_V_ah, initialAdoHcy * MU),
+    Hcy(fn_V_ah - fn_V_cbs - fn_V_ms - fn_V_bhmt, initialHcy * MU) {}
 };
 
 MetabolicModel *model = nullptr;
@@ -223,7 +216,6 @@ int main(int argc, char **argv) {
 
   // hate this
   _5mthf = isc.thf_5m.has_value() ? isc.thf_5m.value() * MU: _5mthf;
-  std::cerr << "Fallat " << _5mthf << std::endl;
 
   static MetabolicModel localModel(
     isc.initialMet.value_or(100),
