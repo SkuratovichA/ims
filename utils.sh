d6c84b21aecf8ddbfd6946cebfbedee98e3d5113
SkuratovichA <skuratovich.aliaksandr@gmail.com>
2023-12-10 21:01:47 +0100

    ccreate different graphs, simulate runs

--- utils.sh
+++ utils.sh
@@ -58,49 +58,57 @@ createDataset() {
   fi
 
   #                 MET     AdoMet  AdoHcy   Hcy   MetinRate 5mthf
-#  runImsWithParams "0.5"   "0.1"    "1"     "1"     "10000" "23.3"
-#  runImsWithParams "0.5"   "0.1"    "0.1"   "0.1"   "10000" "23.3"
-#  runImsWithParams "00.5"   "00.1"  "00.1"  "00.1"  "10000" "23.3"
-
-#  runImsWithParams "0.05"   "1000"  "1"    "1"     "5000"  "23.3" # don't touch this. this is correct
-#  runImsWithParams "0.05"   "1000"  "0.1"  "0.1"   "5000" "2.3" # don't touch this. this is correct
-#  runImsWithParams "0.05"   "1000"  "0.1"  "0.1"  "5000" "3.2" # don't touch this. this is correct
-
-#   runImsWithParams "0.05"   "1000"  "0.1"  "0.1"  "5000" "520000000" # don't touch this. this is correct
-
-   runImsWithParams "50"   "10"  "5"  "2"  "50" # don't touch this. this is correct
-
-#  runImsWithParams "50" "10" "5" "2" "50"
+  runImsWithParams "0.05"   "1000"  "0.1"   "0.1"  "50"  # don't touch this. this is correct
+  runImsWithParams "50"      "10"    "5"     "2"  "20" # don't touch this. this is correct
 
   echo "CREATING PNG FILES from" "$datDir"/*.dat
   for datFile in "$datDir"/*.dat ; do
     local graphFile="${datFile%.dat}.${graphExt}"
+    local graphFile2="${datFile%.dat}LLLL.${graphExt}"
     echo "Creating $graphFile..."
-gnuplot -e "set terminal pdf size 29.7cm, 84.1cm; \
-            set output '${graphFile}'; \
-            set multiplot layout 5,1 title 'Biology Data Overview'; \
-            set datafile separator whitespace; \
-            set lmargin 10; \
-            set rmargin 10; \
-            set tmargin 2; \
-            set bmargin 2; \
-            set title 'Met'; \
-            set ylabel 'µM'; \
-            plot '$datFile' using 1:2 with lines title 'Met'; \
-            set title 'AdoMet'; \
-            set ylabel 'µM'; \
-            plot '$datFile' using 1:3 with lines title 'AdoMet'; \
-            set title 'AdoHcy'; \
-            set ylabel 'µM'; \
-            plot '$datFile' using 1:4 with lines title 'AdoHcy'; \
-            set title 'Hcy'; \
-            set ylabel 'µM'; \
-            plot '$datFile' using 1:5 with lines title 'Hcy'; \
-            set title 'Metin'; \
-            set ylabel 'µM'; \
-            plot '$datFile' using 1:6 with lines title 'Metin'; \
-            unset multiplot; \
-            set output;"
+    gnuplot -e "set terminal pdf size 29.7cm, 84.1cm; \
+                set output '${graphFile}'; \
+                set multiplot layout 5,2 title 'Biology Data Overview'; \
+                set datafile separator whitespace; \
+                set lmargin 10; \
+                set rmargin 10; \
+                set tmargin 2; \
+                set bmargin 2; \
+                set title 'Met'; \
+                set ylabel 'µM'; \
+                plot '$datFile' using 1:2 with lines title 'Met'; \
+                set title 'AdoMet'; \
+                set ylabel 'µM'; \
+                plot '$datFile' using 1:3 with lines title 'AdoMet'; \
+                set title 'AdoHcy'; \
+                set ylabel 'µM'; \
+                plot '$datFile' using 1:4 with lines title 'AdoHcy'; \
+                set title 'Hcy'; \
+                set ylabel 'µM'; \
+                plot '$datFile' using 1:5 with lines title 'Hcy'; \
+                set title 'Metin'; \
+                set ylabel 'µM'; \
+                plot '$datFile' using 1:6 with lines title 'Metin'; \
+                unset multiplot; \
+                set output;"
+
+      gnuplot -e "set terminal pdf size 40.7cm, 20.1cm; \
+                  set output '${graphFile2}'; \
+                  set multiplot layout 1,2 title 'Biology Data Overview'; \
+                  set datafile separator whitespace; \
+                  set lmargin 10; \
+                  set rmargin 10; \
+                  set tmargin 2; \
+                  set bmargin 2; \
+                  set title 'Met'; \
+                  set ylabel 'µM'; \
+                  plot '$datFile' using 1:2 with lines title 'Met'; \
+                  set title 'AdoMet'; \
+                  set ylabel 'µM'; \
+                  plot '$datFile' using 1:3 with lines title 'AdoMet'; \
+                  set title 'AdoHcy'; \
+                  set ylabel 'µM'; \
+                  set output;"
   done
   mv "$datDir"/*.$graphExt "$pngDir"
 }
